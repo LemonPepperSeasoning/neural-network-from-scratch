8dcd70ba2588f1efe8a48bf4cf20db335757938d
LemonPepperSeasoning <takahiro.swe@gmail.com>
2024-02-27 23:07:42 +0900

    addition logic

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::ops;
 use std::fmt;
 use std::vec::Vec;
 
-
+#[derive(Debug, Clone)]
 struct Tensor {
     data: f32,
     grad: f32,
@@ -27,8 +27,8 @@ impl fmt::Display for Tensor {
 impl ops::Add for Tensor {
     type Output = Tensor;
 
-    fn add(self, other: Tensor) -> Tensor {
-        println!("Tensor#add() on (Tensor(data={}), Tensor(data={}))", self.data, other.data);
+    fn add(self, other: Self) -> Self::Output {
+        println!("Tensor#add() on ({}, {})", self, other);
         
         Tensor {
             data: self.data + other.data,
@@ -42,22 +42,32 @@ impl ops::Add for Tensor {
 fn main() {
     println!("Hello, world!");
 
-    let tensor_a = Tensor {
+    let mut tensor_a = Tensor {
         data: 0.001,
         grad: 0.0,
         prev: Vec::new(),
     };
 
-    let tensor_b = Tensor {
-        data: 0.003,
+    let mut tensor_b = Tensor {
+        data: 0.002,
         grad: 0.0,
         prev: Vec::new(),
     };
     
+    let mut tensor_c = Tensor {
+        data: 0.003,
+        grad: 0.0,
+        prev: Vec::new(),
+    };
 
     tensor_a.backward();
 
-    let tensor_c = tensor_a + tensor_b;
+    let tensor_d = tensor_a.clone() + tensor_b.clone();
+    println!("{} + {} = {}", tensor_a, tensor_b, tensor_d);
 
-    println!("{}", tensor_c);
+    let tensor_e = tensor_a.clone() + tensor_c.clone();
+    println!("{} + {} = {}", tensor_a, tensor_c, tensor_e);
+
+    let tensor_f = tensor_b.clone() + tensor_c.clone();
+    println!("{} + {} = {}", tensor_b, tensor_c, tensor_f);
 }
